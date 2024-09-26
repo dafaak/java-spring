c73818be6c259f55ea5b62800a441c3a706b9bf4
Israel <israel.pe@outlook.es>
2024-09-26 13:20:03 -0500

    feat: contructor injection

--- src/main/java/com/dafaak/spring/examples/a1/DependencyInjectionAppLauncher.java
+++ src/main/java/com/dafaak/spring/examples/a1/DependencyInjectionAppLauncher.java
@@ -16,29 +16,36 @@ class YourBusinessClass {
 
     Dependency2 dependency2;
 
+    @Autowired
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        System.out.println("Contructor injection");
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
     public String toString() {
         return "Using " + dependency1 + " and " + dependency2;
     }
 
-    public Dependency2 getDependency2() {
-        return dependency2;
-    }
-
-    @Autowired
-    public void setDependency2(Dependency2 dependency2) {
-        System.out.println("Setter injection - dep1");
-        this.dependency2 = dependency2;
-    }
-
-    public Dependency1 getDependency1() {
-        return dependency1;
-    }
-
-    @Autowired
-    public void setDependency1(Dependency1 dependency1) {
-        System.out.println("Setter injection - dep2");
-        this.dependency1 = dependency1;
-    }
+//    public Dependency2 getDependency2() {
+//        return dependency2;
+//    }
+//
+//    @Autowired
+//    public void setDependency2(Dependency2 dependency2) {
+//        System.out.println("Setter injection - dep1");
+//        this.dependency2 = dependency2;
+//    }
+//
+//    public Dependency1 getDependency1() {
+//        return dependency1;
+//    }
+//
+//    @Autowired
+//    public void setDependency1(Dependency1 dependency1) {
+//        System.out.println("Setter injection - dep2");
+//        this.dependency1 = dependency1;
+//    }
 }
 
 @Component
