1466752d06c4a01b9c74f9d1884ed06070ea7ac1
Israel <israel.pe@outlook.es>
2024-09-26 13:17:14 -0500

    feat: setter injection

--- src/main/java/com/dafaak/spring/examples/a1/DependencyInjectionAppLauncher.java
+++ src/main/java/com/dafaak/spring/examples/a1/DependencyInjectionAppLauncher.java
@@ -11,14 +11,33 @@ import java.util.Arrays;
 
 @Component
 class YourBusinessClass {
-    @Autowired
+
     Dependency1 dependency1;
 
-    @Autowired
     Dependency2 dependency2;
 
     public String toString() {
         return "Using " + dependency1 + " and " + dependency2;
+    }
+
+    public Dependency2 getDependency2() {
+        return dependency2;
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("Setter injection - dep1");
+        this.dependency2 = dependency2;
+    }
+
+    public Dependency1 getDependency1() {
+        return dependency1;
+    }
+
+    @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("Setter injection - dep2");
+        this.dependency1 = dependency1;
     }
 }
 
